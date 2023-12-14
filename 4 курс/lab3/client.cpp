ec1b6aa6f4041d0a62f1790fbb9a15e10faf027e
ARTER <Arter616@gmail.com>
2023-12-14 11:40:47 +0300

    Update client.cpp

--- 4 курс/lab3/client.cpp
+++ 4 курс/lab3/client.cpp
@@ -11,27 +11,6 @@
 #include <iomanip>
 #include <map>
 
-// bool getStatInfoServer() {
-//   FILE *file = popen("sudo netstat -tulpan | grep \"udp\" | grep \"127.0.0.1:8090\" | wc -l", "r");
-//   if (file == nullptr) {
-//       std::cout << "Error executing command shell\n";
-//       exit(2);
-//   }
-//   char tmp;
-//   size_t retVal;
-//   std::string perm;
-//   //while ((retVal = fread(&tmp, 1, sizeof(tmp), file)) != 0) {
-//   //    perm += tmp;
-//   //}
-//   fread(&tmp, 1, sizeof(tmp), file);
-//   perm = tmp;
-//   pclose(file);
-//   if (perm == "1") { // "1\n"
-//     return true;
-//   }
-//   return false;
-// }
-
 std::string DataToServer() {
   FILE *file1 = popen("mail -p | grep -P \'From:|Date:\'", "r");//открывает процесс, создавая канал, производя fork и вызывая командную оболочку, возвращает поток ввода-вывода
   if (file1 == nullptr) {
@@ -45,7 +24,7 @@ std::string DataToServer() {
       perm += tmp;
   }
   pclose(file1);
-  //
+
   FILE *file2 = popen("mail -p | grep -P \'From:\' | wc -l", "r");
   if (file2 == nullptr) {
       std::cout << "Error executing command shell\n";
@@ -54,7 +33,7 @@ std::string DataToServer() {
   while ((retVal = fread(&tmp, 1, sizeof(tmp), file2)) != 0) {
       perm += tmp;
   }
-  //
+
   pclose(file2);
   std::cout << perm;
   std::istringstream ss(perm);
@@ -63,12 +42,12 @@ std::string DataToServer() {
   struct tm tm_time = {0};
   time_t time_;
   while (getline(ss, temp)) {
-    size_t pos = temp.find("Date:"); // Находим позицию "Date:" в строке
+    size_t pos = temp.find("Date:");
         char* p;
         strtol(temp.c_str(), &p, 10);
         if (pos != std::string::npos) {
-            pos += 6; // Перемещаем позицию на позицию после "Date:"
-            std::string dateStr = temp.substr(pos); // Создаем подстроку после "Date:"
+            pos += 6; 
+            std::string dateStr = temp.substr(pos);
 
             strptime(dateStr.c_str(), "%a, %d %b %Y %H:%M:%S", &tm_time);
             time_ = mktime(&tm_time);
@@ -76,9 +55,9 @@ std::string DataToServer() {
             out += temp;
         }
         else {
-            size_t pos1 = temp.find("From:"); // Находим позицию "From:" в строке
+            size_t pos1 = temp.find("From:");
             pos1 += 6;
-            std::string from_ = temp.substr(pos1); // Создаем подстроку после "From"
+            std::string from_ = temp.substr(pos1);
             time_t currentTime = std::time(0);
             if (currentTime - time_ < 604800) {
               out += from_ + "\n";
@@ -116,37 +95,14 @@ int main() {
     inet_pton(AF_INET, "127.0.0.1", &(server_addr.sin_addr));
     //sets port of server
     server_addr.sin_port = htons(8090); // converts from host byte order to network byte order
-    // bool flagSERVER = false;
-    // while (true) {
-    //   if (getStatInfoServer()) {
-    //     break;
-    //   } else {
-    //     if (!flagSERVER) {
-    //       std::cout << "turn on server\n";
-    //       sleep(5);
-    //       flagSERVER = true;
-    //       continue;
-    //     } else {
-    //       std::cout << "server wasn't opened\n";
-    //       std::cout << "Closing client...\n";
-    //       close(client_socket);
-    //       exit(10);
-    //     }
-    //   }
-    // }
 
     char message1[2048];
     memset(message1, 0, sizeof(message1));
     std::string tmp = DataToServer();
     strcpy(message1, tmp.c_str());
 
-    // if (sendto(client_socket, message1, sizeof(message1), 0, (struct sockaddr*)&server_addr, sizeof(server_addr)) == -1){
-    //     std::cout << "Error sending message!\n";
-    //     exit(10);
-    // }
     char buffer[2048];
     memset(buffer, 0, sizeof(buffer));
-    // recvfrom(client_socket, buffer, sizeof(buffer), 0, (struct sockaddr*)&server_addr, &server_addr_length);
 
     while (recvfrom(client_socket, buffer, sizeof(buffer), MSG_DONTWAIT, (struct sockaddr*)&server_addr, &server_addr_length) == -1) {
         std::cout << "Attemptimng access server...\n";
